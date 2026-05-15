af40c1d63d2f7ab5fb49b34baee329d442ca555a
tarjoyoid-hub <tarjoyo.id@gmail.com>
2026-05-15 07:21:02 +0700

    Add files via upload

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 // Service to interact with Google Apps Script Backend
 
 // To use the real backend, replace this URL with your deployed Google Apps Script Web App URL
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyyH561uq19bZtgQs5A5r1snIb6qEozQvfi1J-aXylICGf7-Kd_liZehfIPCFl20xotdA/exec"; 
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzgXkxSgfM_7lEEfxtAwKxffrAGSmeumZoB_UyX5MNyQyEuv9jS_vAR0hypgUeRUkPhMA/exec"; 
 
 export interface FormField {
   id: string;
